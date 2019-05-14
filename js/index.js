e956d282b081ca72611182885e8d9f30a4a605b7
Stacy Williams <49852311+StacyWilliams@users.noreply.github.com>
2019-05-14 14:01:10 -0500

    WIP got first Selector working...not targeting button for them all just setting things up

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
 // Your code goes here
+let btn = document.querySelector(".destination .btn");
+ let counter = 0;
+btn.addEventListener('click', function(event) {
+    counter++;
+    console.log(counter);
+    alert('zoom zoom');
+    })
+
+
+// let link = document.querySelectorAll(".nav [.nav-link]"); 
+// btn.addEventListener('keydown', function(event) {
+//         counter++;
+//         event.target.style.color= 'green';
+//         });
+
+// btn.addEventListener('wheel', function(event) {
+//         counter++;
+//         event.target.style.color= 'green';
+//         });
+
+// btn.addEventListener('focus', function(event) {
+//         counter++;
+//         event.target.style.color= 'green';
+//         });
+        
+// btn.addEventListener('resize', function(event) {
+//     counter++;
+//     event.target.style.color= 'green';
+//         });
+
+//  btn.addEventListener('scroll', function(event) {
+//     counter++;
+//     event.target.style.color= 'green';
+//         });
+
+//  btn.addEventListener('select', function(event) {
+//             counter++;
+//             event.target.style.color= 'green';
+//             });
+
+//  btn.addEventListener('dbclick', function(event) {
+//                 counter++;
+//                 event.target.style.color= 'green';
+//                 });
