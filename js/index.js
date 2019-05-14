24b7c245de378230476c1c8bee8dad56636a5d0b
Stacy Williams <49852311+StacyWilliams@users.noreply.github.com>
2019-05-14 15:02:15 -0500

    WIP this is more difficult then I originally expected

--- js/index.js
+++ js/index.js
@@ -1,45 +1,81 @@
 // Your code goes here
+
+// click
+
 let btn = document.querySelector(".destination .btn");
- let counter = 0;
 btn.addEventListener('click', function(event) {
-    counter++;
-    console.log(counter);
-    alert('zoom zoom');
-    })
+     alert('zoom zoom');
+    });
 
 
-// let link = document.querySelectorAll(".nav [.nav-link]"); 
-// btn.addEventListener('keydown', function(event) {
-//         counter++;
-//         event.target.style.color= 'green';
-//         });
 
-// btn.addEventListener('wheel', function(event) {
-//         counter++;
-//         event.target.style.color= 'green';
-//         });
+// select
 
-// btn.addEventListener('focus', function(event) {
-//         counter++;
-//         event.target.style.color= 'green';
-//         });
+let link = document.querySelector(".nav-link"); 
+link.addEventListener('select', function(event) {
+        counter++;
+        link.onclick = prompt('JavaScript is like magic');
+        });
+
+
+ // wheel
+
+  let scale = 1;
+  const el = document.querySelector('body');
+  el.onwheel = zoom;
+
+body.addEventListener('wheel', function zoom(el) {
+    event.preventDefault();
+  
+    if (event.deltaY < 0) {
+      // Zoom in
+      scale *= event.deltaY * -2;
+    }
+    else {
+      // Zoom out
+      scale /= event.deltaY * 2;
+    }
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    el.style.transform = `scale(${scale})`;
+  }
+ 
+//   focus
+let bus = document.querySelector(".logo-heading");
+
+bus.addEventListener('focus', function(event) {
         
-// btn.addEventListener('resize', function(event) {
-//     counter++;
-//     event.target.style.color= 'green';
-//         });
+        event.target.style.hidden= true;
+        });
+ 
 
-//  btn.addEventListener('scroll', function(event) {
-//     counter++;
-//     event.target.style.color= 'green';
-//         });
+resize
+let heightOutput = document.querySelector('');
+let widthOutput = document.querySelector('');
 
-//  btn.addEventListener('select', function(event) {
-//             counter++;
-//             event.target.style.color= 'green';
-//             });
+function reportWindowSize() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+}
 
-//  btn.addEventListener('dbclick', function(event) {
-//                 counter++;
-//                 event.target.style.color= 'green';
-//                 });
+
+// scroll
+window.onresize = reportWindowSize;
+
+ btn.addEventListener('scroll', function(event) {
+    counter++;
+    event.target.style.color= 'green';
+        });
+
+ btn.addEventListener('select', function(event) {
+            counter++;
+            event.target.style.color= 'green';
+            });
+
+ btn.addEventListener('dbclick', function(event) {
+                counter++;
+                event.target.style.color= 'green';
+                });
